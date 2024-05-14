a5ba90738bd3348e39c238046c615546b90836c7
andrew.sales <andjsales@gmail.com>
2024-05-14 17:51:56 -0500

    initial commit

---
+++ src/components/Translator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Translator() {
+    return (
+        <div>
+            <div className="header">
+                <h2>Subtitle Translator</h2>
+            </div>
+            <div className='body'>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
